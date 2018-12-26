088eb8c9e30892c1fb9826dc4f654c03a3e6983e
aftermath089 <vincentmichael089@gmail.com>
2018-12-27 02:41:38 +0800

    [vincent] add dom p3

--- javascript/main.js
+++ javascript/main.js
@@ -77,11 +77,11 @@ itemterakhir.style.color = 'blue';
 
 //------------------------------------------------
 //DOM Traversing (access child,parent,sibling)
-var itemList=document.querySelector('#items');
+var itemList = document.querySelector('#items');
 //parent
 console.log(itemList.parentNode) //access the parent node
 console.log(itemList.parentNode.parentNode.parentNode)
-itemList.parentNode.style.backgroundColor='#f4f4f4'
+itemList.parentNode.style.backgroundColor = '#f4f4f4'
 //parentNode==parentElement
 //child
 console.log(itemList.childNodes); //nodelist
@@ -93,8 +93,8 @@ console.log(itemList.nextElementSibling);
 //------------------------------------------------
 //CREATING ELEMENT
 var newDiv = document.createElement('div');
-newDiv.className='add-class';
-newDiv.id='add-id'
+newDiv.className = 'add-class';
+newDiv.id = 'add-id'
 newDiv.setAttribute('title', 'this is a title');
 console.log(newDiv);
 
@@ -104,11 +104,63 @@ console.log(newDivText);
 newDiv.appendChild(newDivText);
 console.log(newDiv);
 
-newDiv.style.fontSizez='30px';
+newDiv.style.fontSizez = '30px';
 
 //inserting the element
 var container = document.querySelector('header .container');
 var h1 = document.querySelector('header h1');
- 
-container.insertBefore(newDiv,h1);
+
+container.insertBefore(newDiv, h1);
+//------------------------------------------------
+
+//------------------------------------------------
+//EVENT
+//ada click, dbclick, mousedown, mouseup, mouseenter, mouseleave, mouseout, mouseover, mousemove
+//keydown, keyup, keypress, focus,blur, cut, paste
+function buttonClick() {
+    console.log('button clicked');
+}
+
+var button = document.getElementById('button-clickhere').addEventListener('click', function () {
+    console.log('button clicked via getelementbyid');
+})
+
+var button2 = document.getElementById('button-clickhere2').addEventListener('click', function () {
+    document.getElementById('header-title').textContent = 'title changed via button click'
+})
+
+var button3 = document.getElementById('button-clickhere3').addEventListener('click', function (x) {
+    var output = document.getElementById('output');
+    output.innerHTML = '<h3>' + x.target + '</h3>';
+
+    //x.clientX -> x axis dari mouseclick pada layar keseluruhan
+    //x.clientY -> y axis dari mouseclick ----
+    //x.offsetX -> x axis dari mosueclick pada element tertentu misal button 
+    //x.altKey/shiftKey/ctrlKey -> return true jika bersama click dipencet juga ctrl/shift/alt
+})
+var position = document.getElementById('position');
+var canvas = document.getElementById('empty-card');
+canvas.addEventListener('mousemove', function (e) {
+    position.innerHTML = '<h3> x= ' + e.offsetX + ', y =' + e.offsetY + '</h3>';
+    canvas.style.backgroundColor = "rgb(" + e.offsetX/2 + "," + e.offsetY/2 + ","+40+")";
+})
+
+var itemInput = document.querySelector('input[type="text"');
+var form = document.querySelector('form');
+
+itemInput.addEventListener('keydown',function(e){
+    console.log(e);
+    console.log(e.target.value)
+    position.innerHTML = '<h3>'+e.target.value+'</h3>';
+})
+
+var select = document.querySelector('select');
+select.addEventListener('change',function(e){
+    console.log(e.type);
+    console.log(e.target.value);
+});
+
+form.addEventListener('submit',function(e){
+    e.preventDefault;
+})
 //------------------------------------------------
