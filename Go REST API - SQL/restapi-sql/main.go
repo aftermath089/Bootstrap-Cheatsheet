bdb1fb84b84c2d2d527aea7dfe459afad8a7f7e9
aftermath089 <vincentmichael089@gmail.com>
2020-04-16 20:21:00 +0800

    add routers and model

--- Go REST API - SQL/restapi-sql/main.go
+++ Go REST API - SQL/restapi-sql/main.go
@@ -7,6 +7,12 @@ import(
 	"net/http"
 )
 
+// post model
+type Post struct{
+	ID int
+	Title string
+}
+
 func main(){
 	// open database
 	db, err := sql.Open("mysql","golang:golang-sql@tcp(127.0.0.1:3306)/golang")
@@ -18,6 +24,14 @@ func main(){
 
 	// create router and serve in port 8000
 	router := mux.NewRouter()
+
+	// create endpoints
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	router.HandleFunc("/posts/{id}",updatePost).Methods("PUT")
+
 	http.ListenAndServe(":8000", router)
 
 }
