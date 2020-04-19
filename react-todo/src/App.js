539932279dac8d6af4f392b1820a9d43d002cdb9
aftermath089 <vincentmichael089@gmail.com>
2020-04-19 09:28:46 +0800

    add Todos component to App component

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <Todos/>
     </div>
   );
 }
