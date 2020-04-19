2d817cf07d34d74fbe77705112a69481445f007d
aftermath089 <vincentmichael089@gmail.com>
2020-04-19 17:09:14 +0800

    link route

--- react-todo/src/components/layout/Header.js
+++ react-todo/src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <header style = {HeaderStyle}>
             <h1>Todo List</h1>
+            <Link to="/" style={linkStyle}>Home</Link> | <Link to="/about" style={linkStyle}>About</Link>
         </header>
     )
 }
@@ -15,4 +17,7 @@ const HeaderStyle = {
     padding : '10px'
 }
 
+const linkStyle = {
+    color : '#fff'
+}
 export default Header
