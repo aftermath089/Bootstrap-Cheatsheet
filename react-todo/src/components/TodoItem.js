d5acbdfb339abf8f682b722a22bf1be8f1b9307d
aftermath089 <vincentmichael089@gmail.com>
2020-04-19 10:10:35 +0800

    basic styling

--- react-todo/src/components/TodoItem.js
+++ react-todo/src/components/TodoItem.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types'
 export class TodoItem extends Component {
     render() {
         return (
-        <p>{this.props.todo.title}</p>
+            <div style={{itemStyle}}>
+                <p>{this.props.todo.title}</p>
+            </div>
         )
     }
 }
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+    backgroundColor : ' #f4f4f4'
 }
 
 export default TodoItem
