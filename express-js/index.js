f3ecc950b304c553f0768542ad245bb48b07c5e6
aftermath089 <vincentmichael089@gmail.com>
2020-04-21 05:00:28 +0800

    routing with express static foldeer

--- express-js/index.js
+++ express-js/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('/', (req, res) => {
-    res.send('<h1>hello world</h1>')
-})
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(PORT, () => {
     console.log(`Listen on port ${PORT}`);
