f9e8e7e38046c0ef9712915d958f20fdc0ad72ad
aftermath089 <vincentmichael089@gmail.com>
2020-04-20 23:06:42 +0800

    fundamental server

--- node/index.js
+++ node/index.js
@@ -1,12 +1,56 @@
-const Logger = require('./logger')
+//const Logger = require('./logger')
 // const Person = require('./person.js')
 
 // const person = new Person('name', 30)
 // console.log(person.greeting())
 
-const logger = new Logger()
-logger.on('message', (data) => {
-    console.log('called listener', data);
+// const logger = new Logger()
+// logger.on('message', (data) => {
+//     console.log('called listener', data);
+// })
+
+// logger.log('hello world')
+
+
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    //ROUTE
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            res.end()
+        })
+    }
+
+    if (req.url == '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+            if (err) throw err
+            res.writeHead(200, {'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }
+
+    //REST API
+    if(req.url == '/api/users'){
+        const users = [
+            { name : 'malele', age:12},
+            { name : 'puele', age:14}
+        ]
+
+        res.writeHead(200, {'Content-Type':'application/json'})
+        res.end(JSON.stringify(users))
+    }
 })
 
-logger.log('hello world')
+
+server.listen(PORT, () => {
+    console.log(`listening to port ${PORT}...`)
+})
