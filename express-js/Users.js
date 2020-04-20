39d5248fd222614a51e56aa04ee5218ebc9f7869
aftermath089 <vincentmichael089@gmail.com>
2020-04-21 05:09:04 +0800

    basic api routing

---
+++ express-js/Users.js
@@ -0,0 +1,8 @@
+// mock data
+const Users = [
+    {id:'a',age:3},
+    {id:'b',age:4},
+    {id:'c',age:5}
+]
+
+module.exports = Users
--- express-js/index.js
+++ express-js/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
+const users = require('./Users.js')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-//static folder
-app.use(express.static(path.join(__dirname,'public')))
+//static folder routing
+app.use(express.static(path.join(__dirname, 'public')))
+
+// get all user
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
 
 app.listen(PORT, () => {
     console.log(`Listen on port ${PORT}`);
