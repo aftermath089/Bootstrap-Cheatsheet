a1b24f3afd66eb177cb00451e772d502789c50c6
aftermath089 <vincentmichael089@gmail.com>
2020-04-21 05:35:34 +0800

    basic api routing 2

--- express-js/Users.js
+++ express-js/Users.js
@@ -1,8 +1,8 @@
 // mock data
 const Users = [
-    {id:'a',age:3},
-    {id:'b',age:4},
-    {id:'c',age:5}
+    {id:1,name:'tsu',age:3},
+    {id:2,name:'ba',age:4},
+    {id:3,name: 'sa',age:5}
 ]
 
 module.exports = Users
--- express-js/index.js
+++ express-js/index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 // init middleware
-app.use(logger) 
+app.use(logger)
 
 //static folder routing
 app.use(express.static(path.join(__dirname, 'public')))
@@ -15,6 +15,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 // get all user
 app.get('/api/users', (req, res) => {
     res.json(users)
+})
+
+//get single user
+app.get('/api/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+    if (found) {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg: 'Member not found' })
+    }
 })
 
 app.listen(PORT, () => {
