7ee18367cd23bd6c8a674f9464b96fc79bd72584
aftermath089 <vincentmichael089@gmail.com>
2020-04-22 04:24:53 +0800

    bind to TodoItem

---
+++ vuejs/vue-proj/src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo-item">
+      <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: white;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoratiom: line-through;
+}
+
+.btn-del {
+  background: red;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
