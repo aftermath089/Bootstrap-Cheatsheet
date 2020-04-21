dc735a6c75e158a412089c5eb5cee9474e83e876
aftermath089 <vincentmichael089@gmail.com>
2020-04-22 06:23:15 +0800

    beautify routing

--- vuejs/vue-proj/src/components/layout/Header.vue
+++ vuejs/vue-proj/src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
-      <h1>TodoList</h1>
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 
@@ -11,10 +15,14 @@ export default {
 </script>
 
 <style scoped>
-.header{
-    background: grey;
-    color: white;
-    text-align: center;
-    padding: 10px;
+.header {
+  background: grey;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+    padding: 0;
 }
 </style>
--- vuejs/vue-proj/src/views/Home.vue
+++ vuejs/vue-proj/src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Todos,
+    AddTodo
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id, res.data))
+        .catch(error => console.log(error))
+    },
+
+    addTodo(newData) {
+      const { title, completed } = newData;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title: title,
+          completed: completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(error => console.log(error));
+    }
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => (this.todos = res.data))
+      .catch(err => console.log(err));
   }
-}
+};
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  color: white;
+  background: cornflowerblue;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #2c3e50;
+}
+</style>
