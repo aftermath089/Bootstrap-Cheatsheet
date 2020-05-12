063ae10c5602519685caefecd35590cc0910404f
aftermath089 <vincentmichael089@gmail.com>
2020-05-12 17:29:35 +0800

    using v-for for generating links

--- vuejs-router/vuejs-router-starter-files/travely-router/src/views/Home.vue
+++ vuejs-router/vuejs-router-starter-files/travely-router/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All Destination</h1>
+    <div class="destination">
+      <div v-for="destination in destinations" v-bind:key="destination.name">
+        <router-link v-bind:to="destination.slug">
+          <h2>{{destination.name}}</h2>
+        </router-link>
+        <figure>
+          <router-link v-bind:to="destination.name">
+            <img
+              v-bind:src="require(`@/assets/${destination.image}`)"
+              v-bind:alt="destination.name"
+            />
+          </router-link>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import store from "@/store.js";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      destinations: store.destinations
+    };
   }
 };
 </script>
+
+<style scoped>
+#home{
+  margin: auto;
+}
+
+.destination{
+  display: flex;
+  justify-content: space-between;
+}
+
+img{
+  max-width: 200px;
+}
+</style>
