d463b8c9e83f9e7c1314eb0b084bbd147735745d
aftermath089 <vincentmichael089@gmail.com>
2020-05-12 19:31:35 +0800

    dynamic routing

--- vuejs-router/vuejs-router-starter-files/travely-router/src/views/DestinationDetails.vue
+++ vuejs-router/vuejs-router-starter-files/travely-router/src/views/DestinationDetails.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>the id is {{this.$route.params.id}}</p>
+  <section class="destination">
+    <h1>{{computedDestination.name}}</h1>
+    <div class="destination-details">
+      <img
+        v-bind:src="require(`@/assets/${computedDestination.image}`)"
+        v-bind:alt="computedDestination.name"
+      />
+      <p>{{computedDestination.description}}</p>
+    </div>
+  </section>
 </template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id
+    };
+  },
+  computed: {
+    computedDestination() {
+      return store.destinations.find(
+        destination => destination.id === this.destinationId
+      );
+    }
+  }
+};
+</script>
