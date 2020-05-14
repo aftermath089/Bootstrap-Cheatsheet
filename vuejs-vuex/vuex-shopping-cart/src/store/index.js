5c54d6b825163dbf1c24d2d92b5e7d492118e8bb
aftermath089 <vincentmichael089@gmail.com>
2020-05-14 21:32:12 +0800

    add vuex

---
+++ vuejs-vuex/vuex-shopping-cart/src/store/index.js
@@ -0,0 +1,24 @@
+import vuex from 'vuex'
+import vue from 'vue'
+
+//use middleware
+vue.use(vuex)
+
+new vuex.Store({
+    state: { //equivalent with data
+
+    },
+
+    getters: { //equivalent to computed properties
+
+    },
+
+    actions: { //equivalent to methods properties
+
+    },
+
+    mutations: { //setting and update the state
+
+    }
+
+})
