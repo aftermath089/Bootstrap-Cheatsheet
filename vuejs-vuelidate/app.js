aa9aa967107a97b7d3e349831139d005b74ef99d
aftermath089 <vincentmichael089@gmail.com>
2020-05-15 21:33:51 +0800

    simple validation with vuelidation

--- vuejs-vuelidate/app.js
+++ vuejs-vuelidate/app.js
@@ -1,3 +1,4 @@
+Vue.use(vuelidate.default)
 
 new Vue({
   el: '#app',
@@ -11,7 +12,27 @@ new Vue({
     }
   },
 
+  validations: {  //validation is a reactive properties accesible by using variable $v
+        form: {
+            name: {
+                required: validators.required // form.name is required
+            },
+
+            age: {
+                required: validators.required, //form.age is required
+                integer: validators.integer, //shall be integer
+                between: validators.between(12, 120) //between 12 and 120
+            }
+        }
+    },
+
   methods: {
-  
+    submitForm(){
+        if (!this.$v.form.$invalid) { //emit an invalid if one of the validators isnt satisfied
+            console.log('📝 Form Submitted', this.form)
+          } else {
+            console.log('❌ Invalid form')
+          }
+    }
   }
 })
