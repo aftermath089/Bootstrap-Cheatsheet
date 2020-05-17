991b0bf6c59d3ab55de804bb0690ec2c7d691075
aftermath089 <vincentmichael089@gmail.com>
2020-05-17 18:56:51 +0800

    execute code before every test

---
+++ javascript-jest/javascript/movie.spec.js
@@ -0,0 +1,34 @@
+import movies from './movies'
+
+describe('Favorite Movies', () => {
+  //fresh the array
+  let myMovies = []
+  
+  //before each test initiate myMovies (or the second test will fail)
+  beforeEach(() => {
+    myMovies = [{
+      title: 'Age of Ultron',
+      rate: null
+    }]
+  })
+
+  test('can add a movie', () => {
+    // myMovies = [{
+    //     title: 'Age of Ultron',
+    //     rate: null
+    // }]
+
+    movies.add(myMovies, 'Kung Fury')
+    expect(myMovies).toMatchSnapshot()
+  })
+
+  test('rate a movie', () => {
+    // myMovies = [{
+    //     title: 'Age of Ultron',
+    //     rate: null
+    // }]
+    
+    movies.rate(myMovies[0], 5)
+    expect(myMovies).toMatchSnapshot()
+  })
+})
