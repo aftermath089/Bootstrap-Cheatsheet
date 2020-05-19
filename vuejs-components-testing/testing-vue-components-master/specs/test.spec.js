4c2af6e34a1e201ef3ca5b9b788f10a8ce05a95b
aftermath089 <vincentmichael089@gmail.com>
2020-05-19 23:19:26 +0800

    test add data to listcomponent

--- vuejs-components-testing/testing-vue-components-master/specs/test.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/test.spec.js
@@ -1,5 +1,6 @@
 import {mount} from '@vue/test-utils'
 import TestComponent from '@/test.vue'
+import List from '@/list.vue'
 
 test('mount vue component', () => {
     const wrapper = mount(TestComponent, {
@@ -8,4 +9,18 @@ test('mount vue component', () => {
         }
     })
     expect(wrapper.html()).toMatchSnapshot()
+})
+
+test("ListComponent", () => {
+    const wrapper = mount(List);
+    const movies = wrapper.vm.marvelMovies;
+    
+    wrapper.setData({
+        marvelMovies: [...movies, "Endgame"]
+    });
+    
+    wrapper.vm.$nextTick( () => {
+        expect(wrapper).toMatchSnapshot();
+        done();
+    })
 })
