739540c5bdde4df8e2e9fa6a3c5580a7d5bf79ab
aftermath089 <vincentmichael089@gmail.com>
2020-05-20 04:12:43 +0800

    test simple watch properties

--- vuejs-components-testing/testing-vue-components-master/specs/temp.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/temp.spec.js
@@ -18,4 +18,23 @@ describe('computed properties test', () => {
         wrapper.vm.degrees = 16
         expect(wrapper.vm.fahrenheit).toBe(60.8)
     })
+
+    test('watcher', () => {
+        const wrapper = mount(Temprature, {
+            propsData: {
+              temp: 40
+            }
+          })
+        expect(wrapper.vm.degrees).toBe(40)
+        expect(wrapper.vm.type).toBe('celsius')
+
+        wrapper.setProps({
+            temp: '50f'
+        })
+
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.degrees).toBe(50)
+            expect(wrapper.vm.type).toBe('fahrenheit')
+        })
+    })
 })
