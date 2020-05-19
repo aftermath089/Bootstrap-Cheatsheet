cc4d8582175a73850532d34fa11b940499013050
aftermath089 <vincentmichael089@gmail.com>
2020-05-20 04:42:55 +0800

    test async methods

--- vuejs-components-testing/testing-vue-components-master/specs/github.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/github.spec.js
@@ -7,6 +7,24 @@ describe('methods', () => {
         expect(wrapper.vm.composeUrl(123)).toBe('https://api.github.com/users/123')
     })
 
-  
+
+    test('fetchData', async () => { //make sure its async
+        //mock the data
+        const jsonMock = jest.fn().mockResolvedValue('GITHUB DATA')
+        window.fetch = jest.fn().mockResolvedValue({
+          json: jsonMock
+        })
     
+        const wrapper = mount(GithubCard, {
+          methods: {
+            composeUrl: () => 'url'
+          }
+        })
+    
+        await wrapper.vm.fetchData() //call the async method
+    
+        expect(window.fetch).toHaveBeenCalledWith('url')
+        expect(jsonMock).toHaveBeenCalled()
+        expect(wrapper.vm.data).toBe('GITHUB DATA')
+      })
 })
