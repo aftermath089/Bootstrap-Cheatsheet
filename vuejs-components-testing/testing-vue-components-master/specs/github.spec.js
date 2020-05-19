3355e17c67f6512b58a1d67f89a86de9f8864b10
aftermath089 <vincentmichael089@gmail.com>
2020-05-20 04:28:12 +0800

    test simple methodss

--- vuejs-components-testing/testing-vue-components-master/specs/github.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/github.spec.js
@@ -2,5 +2,8 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 describe('methods', () => {
-
+    test('composeUrl', () => {
+        const {composeUrl} = GithubCard.methods
+        expect(composeUrl(123)).toBe('https://api.github.com/users/123')
+    })
 })
