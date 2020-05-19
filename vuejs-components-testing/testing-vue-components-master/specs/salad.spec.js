37395a94d195dc8eb22836c0e08f37c60c52761b
aftermath089 <vincentmichael089@gmail.com>
2020-05-20 05:48:23 +0800

    test vuex add item to state

--- vuejs-components-testing/testing-vue-components-master/specs/salad.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/salad.spec.js
@@ -4,3 +4,19 @@ import { mount } from '@vue/test-utils'
 
 import SaladBowlComponent from '@/salad-bowl'
 import saladStore from '@/store/salad-store'
+
+//use vuex middleware
+Vue.use(Vuex)
+
+const store = new Vuex.Store(saladStore)
+
+test('store is loaded', () => {
+    const wrapper = mount(SaladBowlComponent, {
+        store
+    })
+
+    store.state.salad.push('cucumber')
+
+    expect(wrapper.vm.salad).toEqual(['cucumber'])
+
+})
