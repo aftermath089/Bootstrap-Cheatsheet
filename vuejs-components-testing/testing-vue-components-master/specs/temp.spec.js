2be07004f55944b02964a8e2b0e3c6df6988122d
aftermath089 <vincentmichael089@gmail.com>
2020-05-20 04:04:30 +0800

    test simple computed properties

--- vuejs-components-testing/testing-vue-components-master/specs/temp.spec.js
+++ vuejs-components-testing/testing-vue-components-master/specs/temp.spec.js
@@ -1,2 +1,21 @@
 import Temprature from '@/temprature'
 import { mount } from '@vue/test-utils'
+
+describe('computed properties test', () => {
+    test('celcius', () => {
+        const wrapper = mount(Temprature)
+        expect(wrapper.vm.celsius).toBe(0)
+        
+        //wrapper.setData({degrees: 23})
+        wrapper.vm.degrees = 23 //because computed properties is reactive we can just change via vm 
+        expect(wrapper.vm.celsius).toBe(23)
+    })
+
+    test('fahrenheit', () => {
+        const wrapper = mount(Temprature)
+        expect(wrapper.vm.fahrenheit).toBe(32)
+        
+        wrapper.vm.degrees = 16
+        expect(wrapper.vm.fahrenheit).toBe(60.8)
+    })
+})
